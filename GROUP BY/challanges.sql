ff1a6b126851af8960609a279bbe0b7b65f1ff20
germandilio <70810123+germandilio@users.noreply.github.com>
2022-08-05 14:39:55 +0300

    Complete GROUP BY Section

---
+++ GROUP BY/challanges.sql
@@ -0,0 +1,9 @@
+SELECT staff_id, COUNT(*) FROM payment GROUP BY staff_id;
+
+SELECT rating, ROUND(AVG(replacement_cost), 4) FROM film GROUP BY rating;
+
+SELECT customer_id, SUM(amount)
+FROM payment
+GROUP BY customer_id
+ORDER BY SUM(amount) DESC
+LIMIT 5;
